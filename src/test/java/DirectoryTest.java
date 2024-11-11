882bdd300688114b42dc00d68ea9922e522fa64d
avinash <avinash.kamath@scalekit.com>
2024-11-11 15:50:02 +0530

    remove proto files

--- src/test/java/DirectoryTest.java
+++ src/test/java/DirectoryTest.java
@@ -30,10 +30,14 @@ public class DirectoryTest {
         updatedAfter = Timestamp.newBuilder().setSeconds(seconds).build();
 
 
-        //Init client
-        String environmentUrl = System.getenv("SCALEKIT_ENVIRONMENT_URL");
-        String  clientId = System.getenv("SCALEKIT_CLIENT_ID");
-        String apiSecret = System.getenv("SCALEKIT_CLIENT_SECRET");
+//        //Init client
+//        String environmentUrl = System.getenv("SCALEKIT_ENVIRONMENT_URL");
+//        String  clientId = System.getenv("SCALEKIT_CLIENT_ID");
+//        String apiSecret = System.getenv("SCALEKIT_CLIENT_SECRET");
+
+        String environmentUrl = "https://well-equipped-boomslang.scalekit.cloud";
+        String  clientId = "skc_26602260880425255";
+        String apiSecret = "test_qxbBaKHcCJch1e4z4efPoNmDB6Mf5jupVdn5hfH7HL7gzq5apGG4tiCkDaHRkmY2";
 
         client = new ScalekitClient(environmentUrl, clientId, apiSecret);
     }
